65c14eb4da5c126515694c6daeb35d86af04712c
webcod0209 <webcod0209@gmail.com>
2024-04-03 17:42:57 +0900

    ⚡️ パフォーマンス改善

--- webpack.config.js
+++ webpack.config.js
@@ -18,16 +18,12 @@ module.exports = {
   module: {
     rules: [
       {
-        // 拡張子が.jsの場合
         test: /\.js$/,
         use: [
           {
-            // BabelによるJsのトランスパイル機能を利用
             loader: "babel-loader",
-            // オプションの設定
             options: {
               presets: [
-                // プリセットを指定することで、ECMAScript5に変換
                 "@babel/preset-env",
               ],
             },
@@ -36,13 +32,10 @@ module.exports = {
       },
 
       {
-        // 拡張子がscssまたはcssの場合の処理
         test: /\.(scss|css)$/,
         use: [
-          // Jsファイルに取り込まれたCSSをDOM要素へ注入するローダー
           "style-loader",
 
-          // mini-css-extract-pluginプラグインを使用してCSSを別ファイルに書き出します。
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
@@ -51,13 +44,9 @@ module.exports = {
           },
   
           {
-            // Jsファイル内に書かれたCSSを取り込む機能
             loader: "css-loader",
-            // オプションの指定
             options: {
-              // CSSのurl()値のメディアを取り込むのを禁止
               url: false,
-              // ソースマップの利用有無
               sourceMap: enabledSourceMap,
 
               // 0 => no loaders (default);
@@ -68,11 +57,8 @@ module.exports = {
           },
           
           {
-            // SassをCSSへトランスパイルする機能
             loader: "sass-loader",
-            // オプションの指定
             options: {
-              // ソースマップの利用有無
               sourceMap: enabledSourceMap,
             },
           },
@@ -81,9 +67,7 @@ module.exports = {
     ],
   },
   plugins: [
-    // CSSファイルを外出しにするプラグイン
     new MiniCssExtractPlugin({
-      // 出力するファイル名の指定
       filename: "style.css",
     }),
   ],
